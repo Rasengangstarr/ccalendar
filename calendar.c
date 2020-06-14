c691057012316e228bee94ea8f47d999fcd8d604
Rasengangstarr <davidouthere@msn.com>
2020-06-14 15:16:58 +0100

    initial commit

---
+++ calendar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc, char** argv)
+{
+	int mode = 0;
+	if (strcmp(argv[1], "-i") == 0) {
+		char date[] = argv[2];
+		char time[] = argv[3];	
+	}
+	return 0;
+}
+
