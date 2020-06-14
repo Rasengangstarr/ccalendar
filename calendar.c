0a9c0320e9e3c5e1113485a79de16407dcf3ced9
Rasengangstarr <davidouthere@msn.com>
2020-06-14 16:04:29 +0100

    basic file io and argument reading

--- calendar.c
+++ calendar.c
@@ -5,9 +5,29 @@ int main(int argc, char** argv)
 {
 	int mode = 0;
 	if (strcmp(argv[1], "-i") == 0) {
-		char date[] = argv[2];
-		char time[] = argv[3];	
+		if (argc < 6){
+			printf("Not enough args...");
+			return 1;
+		} 
+		return addEvent(argv[2],argv[3],argv[4],argv[5]);
 	}
+	return 0;
+}
+
+int addEvent(char *event, char *date, char *timeFrom, char *timeTo)
+{
+	FILE *fp;
+	char writeString[256];
+	strcpy(writeString, date);
+	strcat(writeString, ":");
+	strcat(writeString, timeFrom);
+	strcat(writeString, ":");
+	strcat(writeString, timeTo);
+	strcat(writeString, ":");
+	strcat(writeString, event);
+   	fp = fopen("cal", "w+");
+   	fprintf(fp, writeString);
+   	fclose(fp);
 	return 0;
 }
 
